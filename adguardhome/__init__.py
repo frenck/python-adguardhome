7bf739beff1260eb0c4a1e770c2de7352615a27e
Franck Nijhof <frenck@addons.community>
2019-05-29 20:39:30 +0200

    :shirt: Adds AdGuard exception classes to default module

--- adguardhome/__init__.py
+++ adguardhome/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 """Asynchronous Python client for the AdGuard Home API."""
 
-from .adguardhome import AdGuardHome  # noqa
+from .adguardhome import (  # noqa
+    AdGuardHome,
+    AdGuardHomeConnectionError,
+    AdGuardHomeError,
+)
