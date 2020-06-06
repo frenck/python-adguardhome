db079d3fb48d8f9874fb1a36eeb9a28306f3884e
tasi788 <tdc@tdccc.com.tw>
2020-06-06 20:11:50 +0800

    :bug: fix typo

--- adguardhome/types/check_host.py
+++ adguardhome/types/check_host.py
@@ -8,7 +8,7 @@ from dataclasses_json import dataclass_json
 @dataclass
 class Check_Host:
     reason: str = field(hash=False, repr=True, compare=False, default=None)
-    filter_id: str = field(hash=False, repr=True, compare=False, default=None)
+    filter_id: int = field(hash=False, repr=True, compare=False, default=None)
     rule: str = field(hash=False, repr=True, compare=False, default=None)
     service_name: str = field(hash=False, repr=True, compare=False, default=None)
     cname: str = field(hash=False, repr=True, compare=False, default=None)
