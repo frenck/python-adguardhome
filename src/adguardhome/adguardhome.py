ff204701db574b935d27c750b3537a8cb0580d70
Yuxin Wang <yuxinwang.dev@gmail.com>
2022-01-29 16:16:00 -0500

    Fix async_timeout deprecation warning.

--- src/adguardhome/adguardhome.py
+++ src/adguardhome/adguardhome.py
@@ -132,7 +132,7 @@ class AdGuardHome:
             self._close_session = True
 
         try:
-            with async_timeout.timeout(self.request_timeout):
+            async with async_timeout.timeout(self.request_timeout):
                 response = await self._session.request(
                     method,
                     url,
